8cc29aa506fe8de8d272409b40df445e13b1d496
Xarri Jorge <kodinglife@gmail.com>
2025-03-24 15:20:12 +0000

    Added folders and draft Dashboard & settings pages

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+function Dashboard() {
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold">Dashboard</h1>
+            {/* Add components here */}
+        </div>
+    );
+}
+export default Dashboard;
---
+++ src/pages/Settings.jsx
@@ -0,0 +1,9 @@
+function Settings() {
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold">Settings</h1>
+            {/* Add tabs and forms here */}
+        </div>
+    );
+}
+export default Settings;
