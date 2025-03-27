ff232c82fae444c50b89c532b2a1dc38c1299e7b
Xarri Jorge <kodinglife@gmail.com>
2025-03-27 14:42:12 +0000

    Updated routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Settings from './pages/Settings';
+import Transactions from './pages/Transactions';
+import Cards from './pages/CreditCards';
 
 function App() {
   return (
@@ -9,6 +11,9 @@ function App() {
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/settings" element={<Settings />} />
+        <Route path="/transactions" element={<Transactions/>} />
+        <Route path="/credit-cards" element={<Cards/>}/>
+        <Route path="/accounts" element={<h1>Accounts</h1>} />
       </Routes>
     </Router>
   );
